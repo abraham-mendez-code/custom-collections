b9053e8fcb88beadd4e554bc99db3c0c767d529c
abraham-mendez-debug <abraham.me001@gmail.com>
2025-11-05 14:01:54 -0500

    Add FixedList class

--- src/main/java/com/pluralsight/collection/FixedList.java
+++ src/main/java/com/pluralsight/collection/FixedList.java
@@ -1,4 +1,22 @@
 package com.pluralsight.collection;
 
-public class FixedList {
+import java.util.List;
+
+public class FixedList<T> {
+
+    // class attributes
+    List<T> items;
+    int maxSize;
+
+    // constructor
+    public FixedList(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public void add (T item) {
+
+        if (items.size() < maxSize) {
+            items.add(item);
+        }
+    }
 }
